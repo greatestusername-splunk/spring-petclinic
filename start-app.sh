fcebc934c8352ca394d9cf78c64d3bdcad79038a
jeremyh <jeremyh@splunk.com>
2024-10-03 15:26:45 -0400

    comment

--- start-app.sh
+++ start-app.sh
@@ -3,12 +3,12 @@
 #export APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY=<YOUR_TOKEN_HERE>
 #export APPDYNAMICS_AGENT_ACCOUNT_NAME=<YOUR_APPD_ACCOUNT_NAME_HERE>
 #export APPDYNAMICS_CONTROLLER_HOST_NAME=<YOUR_APPD_URL_HERE>
+#export APPDYNAMICS_JAVA_AGENT_REUSE_NODE_NAME_PREFIX="petclinic-node"
+#export APPDYNAMICS_AGENT_APPLICATION_NAME=petclinic
+#export APPDYNAMICS_AGENT_TIER_NAME=petclinic-tier
 export APPDYNAMICS_CONTROLLER_PORT=443
 export APPDYNAMICS_CONTROLLER_SSL_ENABLED=true
-export APPDYNAMICS_AGENT_APPLICATION_NAME=petclinic
-export APPDYNAMICS_AGENT_TIER_NAME=petclinic-tier
 export APPDYNAMICS_AGENT_NODE_NAME="reuse"
 export APPDYNAMICS_JAVA_AGENT_REUSE_NODE_NAME="true"
-export APPDYNAMICS_JAVA_AGENT_REUSE_NODE_NAME_PREFIX="petclinic-node"
 
-java -javaagent:appdynamics/javaagent.jar -Dappdynamics.opentelemetry.enabled=true -Dotel.traces.exporter=otlp -Dotel.exporter.otlp.traces.endpoint=http://0.0.0.0:4317 -Dotel.resource.attributes="service.name=petclinic-tier,service.namespace=petclinic,deployment.environment=petclinic-ubuntu" -jar target/*.jar
+java -javaagent:appdynamics/javaagent.jar -Dappdynamics.opentelemetry.enabled=true -Dotel.traces.exporter=otlp -Dotel.exporter.otlp.traces.endpoint=http://0.0.0.0:4317 -Dotel.resource.attributes="service.name=$APPDYNAMICS_AGENT_APPLICATION_NAME,service.namespace=petclinic,deployment.environment=$APPDYNAMICS_AGENT_TIER_NAME" -jar target/*.jar
